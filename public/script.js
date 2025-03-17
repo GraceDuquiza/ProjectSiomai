4093929858a3b9fefcc16f7938b4397dc1cfb83d
GraceCoder <duquizagrace03@gmail.com>
2025-03-17 14:56:44 +0100

    Adding google tag manager

--- public/script.js
+++ public/script.js
@@ -7,28 +7,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
-/*
-document.addEventListener('DOMContentLoaded', function () {
-    // Check if the user has already accepted cookies
-    if (!localStorage.getItem('cookies-accepted')) {
-        // Show the cookie banner if the user hasn't accepted cookies
-        document.getElementById('cookie-banner').style.opacity = '1';
-        document.getElementById('cookie-banner').style.bottom = '0';  // Make the banner visible just above the footer
-    }
-
-    // Listen for the "Learn more" link click
-    document.getElementById('cookie-banner').addEventListener('click', function (event) {
-        if (event.target.tagName.toLowerCase() === 'a') {
-            // Set a flag in localStorage to indicate consent has been given
-            localStorage.setItem('cookies-accepted', 'true');
-
-            // Fade the cookie banner out (after the link is clicked)
-            document.getElementById('cookie-banner').style.opacity = '0';
-            document.getElementById('cookie-banner').style.bottom = '-50px'; // Move the banner below the footer
-        }
-    });
-});
-
-*/
-
-    
